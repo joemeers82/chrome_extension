9b329446d458a77b4c6773f23964560c3fdfb5e1
Joseph Jankowski <jjankows@checkpoint.com>
2020-01-27 15:07:56 -0800

    added dropdown to popup

--- background.js
+++ background.js
@@ -1,8 +1,14 @@
-window.bears = {};
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
-	window.bears[request.url] = request.count
-})
+// window.bears = {};
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
+// 	window.bears[request.url] = request.count
+// })
 
-chrome.browserAction.onClicked.addListener(function(tab){
-	chrome.tabs.create({url: 'popup.html'})
-})
+// chrome.browserAction.onClicked.addListener(function(tab){
+// 	chrome.tabs.create({url: 'popup.html'})
+// })
+
+const select = document.querySelector('member_list');
+	// console.log(select);
+	// select.addEventListener('change', (event)=>{
+	// 	console.log(event.target.value);
+	// })
--- popup.js
+++ popup.js
@@ -1,12 +1,31 @@
 document.addEventListener('DOMContentLoaded', function(){
-	
-	const bg = chrome.extension.getBackgroundPage()
+	function tickets(name){
+		const tickets = [];
+		const ticketLength = document.querySelectorAll('.gridcontents_ticketmanagegrid_parent tr').length;
+		console.log(ticketLength);
 
-	Object.keys(bg.bears).forEach(function(url){
-		const div = document.createElement('div')
-		div.textContent =`${url}: ${bg.bears[url]}`
-		document.body.appendChild(div)
-	})
+		const rows = document.querySelectorAll('.gridcontents_ticketmanagegrid_parent tr td:nth-of-type(5)');
+		rows.forEach( el => {
+			let colleague = el.innerText;
+			if(colleague.indexOf(name) !== -1){
+				tickets.push(colleague);
+			}
+			else{
+				el.parentNode.style.display='none';
+			}
+		})
+		let total = tickets.length;
+		console.log(`${tickets[0]} has ${total} active tickets`);
+	}
+
+	
+	// const bg = chrome.extension.getBackgroundPage()
+
+	// Object.keys(bg.bears).forEach(function(url){
+	// 	const div = document.createElement('div')
+	// 	div.textContent =`${url}: ${bg.bears[url]}`
+	// 	document.body.appendChild(div)
+	// })
 
 	// document.querySelector('button').addEventListener('click', onclick, false)
 
